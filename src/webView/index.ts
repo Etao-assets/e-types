13b7c8e61cc4cadeda9cd9bdb49551449c0f1b17
angamba-droid <angamba@etaoassets.com>
2026-03-07 16:45:45 +0530

    webview purpose emum update

--- src/webView/index.ts
+++ src/webView/index.ts
@@ -2,4 +2,6 @@ export enum WebviewScreenPurpose {
   UCC2FA = 'ucc2fa',
   ORDER_PLACE_2FA = 'order_place_2fa',
   ORDER_CANCEL_2FA = 'order_cancel_2fa',
+  ENACH_MANDATE = 'enach_mandate',
+  SXP_REG_2FA = 'sxp_reg_2fa',
 }
