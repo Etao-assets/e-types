53f6577f594e63782bd6b8c569302e8ac8c6fce8
parteek-etao <parteek@etaoassets.com>
2026-03-16 15:24:51 +0530

    order redeem webpurpose added

--- src/webView/index.ts
+++ src/webView/index.ts
@@ -2,6 +2,7 @@ export enum WebviewScreenPurpose {
   UCC2FA = 'ucc2fa',
   ORDER_PLACE_2FA = 'order_place_2fa',
   ORDER_CANCEL_2FA = 'order_cancel_2fa',
+  ORDER_REDEEEM_2FA = 'order_redeem_2fa',
   ENACH_MANDATE = 'enach_mandate',
   SXP_REG_2FA = 'sxp_reg_2fa',
 }
